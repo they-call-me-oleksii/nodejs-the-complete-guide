ca930bd5b73174bb2df5fc1cdd0e1845bcc0a410
Oleksii Yastrebtsev <aldevid9@gmail.com>
2024-09-19 10:14:36 +0300

    Assignment-4 rewrite to ejs

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const homeRouter = require("./routes/home");
 const usersRouter = require("./routes/users");
 
 const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 // Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(usersRouter);
 app.use(homeRouter);
+app.use((_req, res, _next) => {
+  res.status(404).render("404", { pageTitle: "404" });
+});
 
 app.listen(3000);
